b6484ea68f9d21bc2c9103f3d75751c704e0fcdc
Dhia M <merzougui.dhia@gmail.com>
2022-05-16 19:01:30 +0200

    Added layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+
+function Layout ({children}: {children: React.ReactNode}){
+    return(
+        <>
+            <Header />
+
+            <main>
+                {children}
+            </main>
+
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
