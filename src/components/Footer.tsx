fbcbd6f27c89bd00967f86a6f27a81c3f27524fa
Dhia M <merzougui.dhia@gmail.com>
2022-05-16 19:01:10 +0200

    Added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "../style/Footer.module.css"
+
+function Footer (){
+    return(
+        <footer>
+            <p>All Rights Reservd</p>
+        </footer>
+    )
+}
+
+export default Footer;
