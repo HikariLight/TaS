34e13e97d1e5dc8d27f57461cea3140a86f50cac
Dhia M <merzougui.dhia@gmail.com>
2022-05-19 20:07:57 +0200

    Minor gallery page CSS modifications

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import galleryStyle from "../style/Gallery.module.css"
+
+function Gallery(){
+    return(
+        <Layout>
+            <h1 className = {galleryStyle.title}>Gallery</h1>
+            <p>To be built...</p>
+        </Layout>
+    )
+}
+
+export default Gallery
