86f77b4c6065784d495edc60f5d3871c7d0a86c9
Dhia M <merzougui.dhia@gmail.com>
2022-05-31 05:02:57 +0200

    Added signup logic

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,26 +1,61 @@
+import { useState, MouseEvent } from "react"
 import Layout from "../components/Layout"
 import signUpStyle from "../style/Auth.module.css"
 
-function SignUp(){
-    return(
-        <Layout>
-            <h1 className = {signUpStyle.title}>Sign Up</h1>
+function SignUp() {
 
-            <form className = {signUpStyle.form} action="">
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmuit = async (e: MouseEvent) => {
+
+        e.preventDefault();
+
+        let userData = {
+            username,
+            email,
+            password
+        }
+
+        const url = "http://127.0.0.1:3001/signup";
+        const options = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData),
+        }
+
+        try {
+            await fetch(url, options)
+                .then((response) => response.json())
+                .then((result) => { console.log(result) });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <Layout>
+            <h1 className={signUpStyle.title}>Sign Up</h1>
+
+            <form className={signUpStyle.form} autoComplete="off">
                 <label>
                     Username:
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
                 </label>
                 <label>
                     E-Mail:
-                    <input type="text" name="email" />
+                    <input type="text" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </label>
                 <label>
                     Password:
-                    <input type="text" name="password" />
+                    <input type="password" name="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
                 </label>
 
-                <button className = {signUpStyle.submitButton}>Submit</button>
+                <button className={signUpStyle.submitButton} onClick={(e) => { handleSubmuit(e) }}>Submit</button>
 
             </form>
         </Layout>
